35e81cb80f48ef7220f6bc8e0ec2f23928593f8d
vitoscape <vit2001g@gmail.com>
2024-02-04 19:17:59 +0300

    Menu system completed

--- matrix_operations.c
+++ matrix_operations.c
@@ -6,6 +6,7 @@
 
 #include "matrix_operations.h"
 
+
 // Главное меню
 void start_screen() {
     uint8_t continue_flag = 0; // Флаг продолжения
@@ -13,31 +14,24 @@ void start_screen() {
     while(!continue_flag) {
         continue_flag = 1;  // Взвод флага на пропуск
         system("clear");    // Очистка экрана
-        int input = 0;  // Вводимый символ
+        char input = 0;  // Вводимый символ
 
         printf("Matrix operations.\nChoose operation and press Enter key.\n\n");
         printf("%d. Multiplication\n", MUL_POINT);
         printf("%d. Exit\n\n>>", EXT_POINT);
 
-        scanf("%d", &input); // Ввод
+        scanf("%c", &input); // Ввод
 
-        if (input == MUL_POINT) {
+        // Выбор пункта меню
+        if (ctoi(input) == MUL_POINT) {
             multiplication();   // Выхов функции умножения
-        } else if (input == EXT_POINT) {
+        } else if (ctoi(input) == EXT_POINT) {
             system("clear");    // Очистка экрана
-
             printf("Thank you for using this program.\n");
-            printf("Press any key to continue...");
-
-            getc(stdin);          // Для нажатия клавиши
             exit(0);            // Выход из программы
         } else {
             continue_flag = 0;  // Сброс флага
-
             printf("\nEnter number between %d to %d:\n", MUL_POINT, EXT_POINT);
-            printf("Press any key to continue...");
-
-            scanf("%d", &input);    // Для нажатия клавиши
         }
     }
 }
--- matrix_operations.h
+++ matrix_operations.h
@@ -5,6 +5,9 @@
 #define MUL_POINT 1 // Номер пункта Умножение
 #define EXT_POINT 2 // Номер пункта Выход
 
+// Макросы
+#define ctoi(c) c - '0'     // Перевод char в int
+
 // Прототипы функций
 void start_screen(void);    // Начальный экран, главное меню
 void multiplication(void);  // Умножение матриц

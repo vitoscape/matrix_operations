188d2b780a30255425eb2553dbcaeca0b4ad25cf
vitoscape <vit2001g@gmail.com>
2024-02-05 16:08:59 +0300

    Added pthr_data struct

--- matrix_operations.h
+++ matrix_operations.h
@@ -8,6 +8,16 @@
 // Макросы
 #define ctoi(c) c - '0'     // Перевод char в int
 
+// Структура для данных потока
+typedef struct {
+    uint8_t m1_line;    // Строка первой матрицы
+    uint8_t m2_column;  // Столбец второй матрицы
+    float** m1;
+    float** m2; // Указатели на матрицы
+    float** mr;
+} pthr_data;
+
+
 // Прототипы функций
 void start_screen(void);    // Начальный экран, главное меню
 void multiplication(void);  // Умножение матриц

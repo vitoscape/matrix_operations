2f9cd4c525f2b4b4bf3c96d3da91fadaae4a448b
vitaliy <vit2001g@gmail.com>
2024-07-14 16:14:54 +0300

    Translate comments

--- matrix_operations.h
+++ matrix_operations.h
@@ -2,27 +2,29 @@
 #define __MATRIX_OPERATIONS_H
 
 
-#define MUL_POINT 1 // Номер пункта Умножение
-#define EXT_POINT 2 // Номер пункта Выход
+#define MUL_POINT 1		// Multiplication menu number
+#define EXT_POINT 2		// Exit menu number
 
-#define DEBUG       // Вывод дебажной информации
+#define DEBUG			// Print debug information
 
-// Макросы
-#define ctoi(c) c - '0'     // Перевод char в int
+// Macroses
+#define ctoi(c) c - '0'	// Translate char to int
 
-// Структура для данных потока
+
+// Thread data structure
 typedef struct {
-    uint8_t m1_line;    // Номер строки первой матрицы
-    uint8_t m2_column;  // Номер столбца второй матрицы
-    uint8_t m_size;     // Кол-во элементов в строке/столбце
+    uint8_t m1_line;	// 1st matrix line number
+    uint8_t m2_column;	// 2nd matrinx column number
+    uint8_t m_size;		// Elements count in line/column
     float** m1;
-    float** m2; // Указатели на матрицы
+    float** m2;			// Matrix pointers
     float** mr;
 } pthr_data;
 
 
-// Прототипы функций
-void start_screen(void);    // Начальный экран, главное меню
-void multiplication(void);  // Умножение матриц
+// Prototypes
+void* thr_mul(void* thr_data);	// Thread function
+void start_screen(void);		// Start screen, main menu
+void multiplication(void);		// Matrix multiplication mode
 
 #endif
